d047657d862f51db6459798e37e4201423b757a2
Shabbir Nayeem <44625465+shabbirnayeem@users.noreply.github.com>
2021-01-17 18:55:17 -0500

    Update main.py
    Adding Error handling with try and except.
    The program only allows letters, no numbers.

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 # TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -29,13 +7,18 @@ nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter Word: ").upper()
+# Using try and except to catch errors
+running = True
 
-code = [nato_alphabet[letter] for letter in user_input]
+while running:
+    user_input = input("Enter Word: ").upper()
+    try:
+        code = [nato_alphabet[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letters in hte alphabet please")
+    else:
+        print(code)
 
-print(code)
 
-
-# code = [nato_alphabet.values() for letter in user_input if letter in nato_alphabet.keys()]
 
 
